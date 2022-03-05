68947016ccbde9ba0034fe61129e301c3df4ba37
dairelavelle <dairelavelle@gmail.com>
2022-03-05 18:02:29 +0000

    Adding the whole ass project

---
+++ Model/Builder/IBookBuilder.cs
@@ -0,0 +1,23 @@
+﻿using book;
+using System;
+using System.Collections.Generic;
+
+namespace Builder {
+    public abstract class IBookBuilder {
+        protected abstract void AddCategory();
+        protected abstract void AddTitle(string title);
+        protected abstract void AddISBN(string isbn);
+        protected abstract void AddISSN(string issn);
+        protected abstract void AddPublisher(string publisher);
+        protected abstract void AddGenre(string genre);
+        protected abstract void AddDescription(string description);
+        protected abstract void AddAuthor(string author);
+        protected abstract void AddSeries(string series);
+        protected abstract void AddEdition(string edition);
+        protected abstract void AddVolume(string volume);
+        protected abstract void AddIssue(string issue);
+        protected abstract void AddPublished(DateTime published);
+        //public abstract Book buildBook(string[] arr);
+        public abstract Book buildBook(string arr);
+    }
+}
---
+++ Model/Builder/TextbookBookBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using book;
+
+namespace Builder {
+
+	public class TextbookBookBuilder : IBookBuilder {
+		private Textbook _book = new Textbook();
+		public Book Book { get { return _book; } }
+
+		protected override void AddCategory() { _book.Category = "Textbook"; }
+		protected override void AddTitle(string title) { _book.Title = title; }
+		protected override void AddISBN(string isbn) { _book.ISBN = isbn; }
+		protected override void AddPublisher(string publisher) { _book.Publisher = publisher; }
+		protected override void AddGenre(string genre) { _book.Genre = genre; }
+		protected override void AddDescription(string description) { _book.Description = description; }
+		protected override void AddAuthor(string author) { _book.AddAuthor(author); }
+		protected override void AddEdition(string edition) { _book.Edition = Int32.Parse(edition); }
+		protected override void AddPublished(DateTime published) { _book.Published.Add(published); }
+
+		//This method is for textbooks being added by a user
+		//The string format is:
+		//"AUTHOR:author name;TITLE:book title;ISBN:book's ISBN"
+		//it is unordered. Adding additional dates of publication or authors will add to a list
+		//Adding additional to any other field will overwrite with the latest  
+
+        public override Book buildBook(string arr) {
+			AddCategory();
+            string[] fields = arr.Split(';');
+			foreach (string field in fields) {
+				string fieldIdentifier = field.Split(':')[0];
+				string fieldValue = field.Split(':')[1];
+				switch (fieldIdentifier) {
+					case "AUTHOR": AddAuthor(fieldValue); break;
+					case "TITLE": AddTitle(fieldValue); break;
+					case "ISBN": AddISBN(fieldValue); break;
+					case "PUBLISHER": AddPublisher(fieldValue); break;
+					case "GENRE": AddGenre(fieldValue); break;
+					case "DESCRIPTION": AddDescription(fieldValue); break;
+					case "EDITION": AddEdition(fieldValue); break;
+					case "DATE": AddPublished(DateTime.Parse(fieldValue)); break;
+				}
+			}
+			return this._book;
+        }
+
+        protected override void AddISSN(string issn) {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddSeries(string series) {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddVolume(string volume) {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddIssue(string issue) {
+            throw new NotImplementedException();
+        }
+    }
+}
